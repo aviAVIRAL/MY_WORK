b317f6c7a81b7481ce6f255d1bdb051e20e99b11
avi_Aviral <140230856+aviAVIRAL@users.noreply.github.com>
2023-09-20 23:31:42 +0530

    yes

--- array/12_Find missing number in an array.py
+++ array/12_Find missing number in an array.py
@@ -16,3 +16,56 @@ def missing_value(arr):
 arr =[0, 1, 2, 3,     5 ,6 ]
 ans = missing_value(arr )
 print (ans)
+
+
+# 2 . better  tc = o(2n)  sc = o(n)
+
+def find_missing_number(arr):
+    n = len(arr)
+    Hash = [0] * (n + 20)
+
+    for i in range(n):
+        Hash[arr[i]] += 1
+
+    for i in range(1, n + 20):
+        if Hash[i] == 0:
+            break
+
+    return i
+
+arr =[1,2,3,5]
+ans =find_missing_number(arr)
+print(ans)
+print()
+arr =[1,2,1,3,5]
+ans =find_missing_number(arr)
+print(ans)
+print()
+arr =[3,0,1,2]
+ans =find_missing_number(arr)
+print(ans)
+
+
+#  3. optimal   tc = o(n)  sc = o(1)
+
+
+def missingNumber(arr , N): 
+
+    summation = (N * (N + 1)) // 2   # here N is not a size of arr :-len(arr) & N is bigest number that its .it will predefine in quetionn gfg or leedcode  
+
+
+    X = sum(arr)
+
+    missinG_ELEMENT = summation - X
+    
+    return missinG_ELEMENT
+
+N = 5
+arr =[1,2,3,5]
+ans =missingNumber(arr, N)
+print(ans)
+print()
+N = 5
+arr =[0,1,2,3,5]
+ans =missingNumber(arr, N )
+print(ans)
